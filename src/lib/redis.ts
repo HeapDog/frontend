30f4dd1c6bfa053de0b1f93f3695eb7aaa2bddb7
parthokr <partho.kr@proton.me>
2026-05-27 17:48:40 +0600

    chore(lazy): set lazyConnect to true

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,10 +1,11 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
+  host: process.env.REDIS_HOST || "localhost",
+  port: parseInt(process.env.REDIS_PORT || "6379"),
   password: process.env.REDIS_PASSWORD,
   db: 0,
+  lazyConnect: true,
 });
 
 export default redis;
